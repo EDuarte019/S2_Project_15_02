0a349c70941e83782dd3916ec86b896727de0948
Evelyn Duarte <eduarte019@west-mec.org>
2019-04-23 17:33:50 -0700

    Final Commit
    Created function for being able to get the sum of the total amount of cost. Ready for review.

--- dl_expense.js
+++ dl_expense.js
@@ -32,11 +32,13 @@
 */
 // When the page loads, there is an anonymous function that will control the submit button by defining that the submit button should equal to the validateSummary function when clicked.
 window.addEventListener("load", function () {
-      var changingCells = document.querySelectorAll('input[class="sum"]');
+      var changingCells = document.querySelectorAll('table#travelExp input.sum');
       for (var i = 0; i < changingCells.length; i++) {
             changingCells[i].onchange = calcExp;
       }
-      document.getElementById("submitButton").onclick = validateSummary;
+      document.getElementById("submitButton").onclick = function () {
+            validateSummary();
+      };
 });
 // The function validateSummary has a variable that will get the summary id. Then, if validate is missing, a message will pop up. If not, there will be nothing which is defined by an empty text string.
 function validateSummary() {
@@ -47,30 +49,31 @@ function validateSummary() {
             validate.setCustomValidity("");
       };
 }
-
+//The function has a parameter of sumClass that is defined when getting sumClass by its class name. Then, sum total has an initial value of 0. Then, created a for loop for the sumFields that has an if with a condiion of itemValue as a number, if it is, it will add sumTotal to itemvalue.
 function calcClass(sumClass) {
-      var sumFields = document.getElementsByClassName("sumClass");
+      var sumFields = document.getElementsByClassName(sumClass);
       var sumTotal = 0;
       for (var i = 0; i < sumFields.length; i++) {
-            var itemValue = parseFloat(sumFields);
-            if (itemValue === isNaN()) {
-
-            } else {
-
+            var itemValue = parseFloat(sumFields[i].value);
+            if (!isNaN(itemValue)) {
+                  sumTotal += itemValue;
             }
       }
       return sumTotal;
-};
-
+}
+//The function calcExp has a variable of an object collections. Then, created a for loop for the expTable. element is retrieved by their id value. Then, set equal to the formatNumber function with parameters as a function with parameters of respected values. Also, two decimal places.
 function calcExp() {
-      var expTable = document.querySelectorAll(tr);
+      var expTable = document.querySelectorAll("table#travelExp tbody tr");
       for (var i = 0; i < expTable.length; i++) {
-            calcClass(dateIndex) = subtotalIndex;
+            document.getElementById("subtotal" + i).value = formatNumber(calcClass("date" + i), 2);
       }
-      var transTotal = calcClass(trans);
-      var lodgeTotal = calcClass(lodge);
-      var mealTotal = calcClass(meal);
-      var otherTotal = calcClass(other);
+      document.getElementById("transTotal").value = formatNumber(calcClass("trans"), 2);
+      document.getElementById("lodgeTotal").value = formatNumber(calcClass("lodge"), 2);
+      document.getElementById("mealTotal").value = formatNumber(calcClass("meal"), 2);
+      document.getElementById("otherTotal").value = formatNumber(calcClass("other"), 2);
+
+
+
 }
 
 
